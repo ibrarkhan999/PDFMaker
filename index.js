76fc471055cbc5583a0d6bc2fd394d46af4982aa
ibrarkhan999 <999ibrarkhan@gmail.com>
2026-09-18 20:29:45 +0500

    Stable version on RN 0.76.9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+const originalHandler = ErrorUtils.getGlobalHandler();
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  console.log('GLOBAL ERROR:', error.message);
+  console.log('STACK:', error.stack);
+  originalHandler(error, isFatal);
+});
 
 AppRegistry.registerComponent(appName, () => App);
